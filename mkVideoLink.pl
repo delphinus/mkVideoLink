f3a054c196ec9263c5ea3d58564942748e5d0095
delphinus35 <delphinus@remora.cx>
2012-05-20 14:41:07 +0900

    test symlink

--- mkVideoLink.pl
+++ mkVideoLink.pl
@@ -4,6 +4,9 @@ use warnings;
 use HTTP::Date qw!time2iso!;
 use Path::Class;
 
+my $SRC_DIR = dir('G:/BD/Videos');
+my $DESC_DIR = dir('C:/Dropbox/Videos');
+
 my @videos = sort {
     $b->stat->mtime <=> $a->stat->mtime
 } grep { /\.m4v$/ } dir('G:/BD/Videos')->children;
@@ -11,7 +14,12 @@ my @videos = sort {
 
 my $sum_size = 0;
 for my $v (@videos) {
+    ref $v or next;
+    $v->basename eq 'test.m4v' or next;
     say $v->basename . " => " . time2iso($v->stat->mtime);
+    my $symlink = $DESC_DIR->file($v->basename);
+    my @args = ('mklink', $symlink->sringify, $v->stringify);
+    say join ' ', @args;
     $sum_size += $v->stat->size;
 }
 
