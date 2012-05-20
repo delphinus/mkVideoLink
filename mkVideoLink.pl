da589ba6c1c30fedf01820028d3974373ea66a77
delphinus35 <delphinus@remora.cx>
2012-05-20 14:13:29 +0900

    recent 20 files

--- mkVideoLink.pl
+++ mkVideoLink.pl
@@ -1,7 +1,15 @@
 #!/usr/bin/env perl
 use 5.12.0;
 use warnings;
+use Date::Manip;
 use Path::Class;
 
-my @videos = grep { /\.m4v$/ } dir('G:/BD/Videos')->children;
-say scalar @videos;
+my @videos = sort {
+    $b->stat->mtime <=> $a->stat->mtime
+} grep { /\.m4v$/ } dir('G:/BD/Videos')->children;
+@videos = @videos[0 .. 19]; # 最新 20 個のリンクを作成する
+
+for my $v (@videos) {
+    say $v->basename . " => " .
+        UnixDate 'epoch ' . $v->stat->mtime => '%Y/%m/%d %H:%M:%S';
+}
